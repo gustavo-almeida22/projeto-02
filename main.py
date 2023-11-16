2d4850067ba42dc6fb1e0284a0d158a6e3b18702
gustavo-almeida22 <151071494+gustavo-almeida22@users.noreply.github.com>
2023-11-16 16:55:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+# criar funções para as notas
+
+nome = str(input('Digite seu nome: '))
+alunoOuProf = str(input('Você é aluno ou professor? '))
+if alunoOuProf=='Aluno' or 'aluno':
+    print('''Olá, eu sou o programa feito para você aluno,
+descobrir suas médias por área de conhecimento.''')
+    print('Agora digite suas notas por período em cada matéria:')
+elif alunoOuProf=='professor' or 'Professor' or 'professora' or 'Professora':
+    print('''Olá, eu sou o programa feito para você professor(a),
+descobrir as médias por área de conhecimento de seus alunos.''')
+    print('Agora digite as notas por período em cada matéria:')
+
+# Matérias
+print('1º BIMESTRE:')
+mt1 = float(input('Matemática: '))
+lp1 = float(input('Língua Portuguesa: '))
+li1 = float(input('Língua Inglesa: '))
+le1 = float(input('Língua Espanhola: '))
+arte1 = float(input('Arte: '))
+edFis1 = float(input('Educação Física: '))
+geo1 = float(input('Geografia: '))
+his1 = float(input('História: '))
+fil1 = float(input('Filosofia: '))
+soc1 = float(input('Sociologia: '))
+fis1 = float(input('Física: '))
+qui1 = float(input('Química: '))
+bio1 = float(input('Biologia: '))
+
+print('2º BIMESTRE:')
+mt2 = float(input('Matemática: '))
+lp2 = float(input('Língua Portuguesa: '))
+li2 = float(input('Língua Inglesa: '))
+le2 = float(input('Língua Espanhola: '))
+arte2 = float(input('Arte: '))
+edFis2 = float(input('Educação Física: '))
+geo2 = float(input('Geografia: '))
+his2 = float(input('História: '))
+fil2 = float(input('Filosofia: '))
+soc2 = float(input('Sociologia: '))
+fis2 = float(input('Física: '))
+qui2 = float(input('Química: '))
+bio2 = float(input('Biologia: '))
+
+print('3º BIMESTRE:')
+mt3 = float(input('Matemática: '))
+lp3 = float(input('Língua Portuguesa: '))
+li3 = float(input('Língua Inglesa: '))
+le3 = float(input('Língua Espanhola: '))
+arte3 = float(input('Arte: '))
+edFis3 = float(input('Educação Física: '))
+geo3 = float(input('Geografia: '))
+his3 = float(input('História: '))
+fil3 = float(input('Filosofia: '))
+soc3 = float(input('Sociologia: '))
+fis3 = float(input('Física: '))
+qui3 = float(input('Química: '))
+bio3 = float(input('Biologia: '))
+
+print('4º BIMESTRE:')
+mt4 = float(input('Matemática: '))
+lp4 = float(input('Língua Portuguesa: '))
+li4 = float(input('Língua Inglesa: '))
+le4 = float(input('Língua Espanhola: '))
+arte4 = float(input('Arte: '))
+edFis4 = float(input('Educação Física: '))
+geo4 = float(input('Geografia: '))
+his4 = float(input('História: '))
+fil4 = float(input('Filosofia: '))
+soc4 = float(input('Sociologia: '))
+fis4 = float(input('Física: '))
+qui4 = float(input('Química: '))
+bio4 = float(input('Biologia: '))
+
+# Cálculos
+
+mediaMatematica = (mt1 + mt2 + mt3 + mt4)/4
+mediaPortugues = (lp1 + lp2 + lp3 + lp4)/4
+mediaIngles = (li1 + li2 + li3 + li4)/4
+mediaEspanhol = (le1 + le2 + le3 + le4)/4
+mediaArte = (arte1 + arte2 + arte3 + arte4)/4
+mediaEdFisica = (edFis1 + edFis2 + edFis3 + edFis4)/4
+mediaGeografia = 
+mediaHistoria = 
+mediaFilosofia = 
+mediaSociologia = 
+mediaFisica = 
+mediaQuimica = 
+mediaBiologia = 
+
+
+# Média por área
+
+mediaNatureza = 
+mediaHumanas = 
+mediaLinguagens = 
+mediaCompleta = ()/13
