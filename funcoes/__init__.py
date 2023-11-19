905fbbeb62f94e2425e86fd5b1c45f1a5be9be20
gustavo-almeida22 <151071494+gustavo-almeida22@users.noreply.github.com>
2023-11-19 15:48:36 -0300

    CONFIRME

--- funcoes/__init__.py
+++
@@ -1,2 +0,0 @@
-def notaNatureza(nota1, nota2, nota3):
-    return ((notaNatureza)/3)
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# criar funções para as notas
+def calculaNotas (nota1, nota2, nota3, nota4):
+  return (nota1 + nota2 + nota3 + nota4)/4
+
+def calculaNotas2 (nota1, nota2, nota3):
+  return (nota1 + nota2 + nota3)/3
+
+def calculaNotas3 (nota1, nota2, nota3, nota4, nota5):
+  return (nota1 + nota2 + nota3 + nota4 + nota5)/5
+
+def calculaNotasGeral (nota1, nota2, nota3, nota4, nota5, nota6, nota7, nota8, nota9, nota10, nota11, nota12, nota13):
+  return (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10 + nota11 + nota12 + nota13)/13
 
 nome = str(input('Digite seu nome: '))
 alunoOuProf = str(input('Você é aluno ou professor? '))
-if alunoOuProf=='Aluno' or 'aluno':
-    print(f'''Olá, {nome}. Eu sou o programa feito para você aluno,
+if alunoOuProf=='aluno':
+  print(f'''Olá, {nome}. Eu sou o programa feito para você aluno,
 descobrir suas médias por área de conhecimento.''')
-    print('Agora digite suas notas por período em cada matéria:')
-elif alunoOuProf=='professor' or 'Professor' or 'professora' or 'Professora':
-    print(f'''Olá, {nome}. Eu sou o programa feito para você professor(a),
+  print('Agora digite suas notas por período em cada matéria:')
+elif alunoOuProf== 'professor':
+  print(f'''Olá, professor(a) {nome}. Eu sou o programa feito para você professor(a),
 descobrir as médias por área de conhecimento de seus alunos.''')
-    aluno = str(input('Digite o nome do aluno: '))
-    print('Agora digite as notas por período em cada matéria:')
+  aluno = str(input('Digite o nome do aluno: '))
+  print('Agora digite as notas por período em cada matéria:')
 
 # Matérias
-print('1º BIMESTRE:')
+print('''
+1º BIMESTRE:
+''')
 mt1 = float(input('Matemática: '))
 lp1 = float(input('Língua Portuguesa: '))
 li1 = float(input('Língua Inglesa: '))
@@ -28,7 +40,9 @@ fis1 = float(input('Física: '))
 qui1 = float(input('Química: '))
 bio1 = float(input('Biologia: '))
 
-print('2º BIMESTRE:')
+print('''
+2º BIMESTRE:
+''')
 mt2 = float(input('Matemática: '))
 lp2 = float(input('Língua Portuguesa: '))
 li2 = float(input('Língua Inglesa: '))
@@ -43,7 +57,9 @@ fis2 = float(input('Física: '))
 qui2 = float(input('Química: '))
 bio2 = float(input('Biologia: '))
 
-print('3º BIMESTRE:')
+print('''
+3º BIMESTRE:
+''')
 mt3 = float(input('Matemática: '))
 lp3 = float(input('Língua Portuguesa: '))
 li3 = float(input('Língua Inglesa: '))
@@ -58,7 +74,9 @@ fis3 = float(input('Física: '))
 qui3 = float(input('Química: '))
 bio3 = float(input('Biologia: '))
 
-print('4º BIMESTRE:')
+print('''
+4º BIMESTRE:
+''')
 mt4 = float(input('Matemática: '))
 lp4 = float(input('Língua Portuguesa: '))
 li4 = float(input('Língua Inglesa: '))
@@ -75,41 +93,58 @@ bio4 = float(input('Biologia: '))
 
 # Cálculos
 
-mediaMatematica = (mt1 + mt2 + mt3 + mt4)/4
-mediaPortugues = (lp1 + lp2 + lp3 + lp4)/4
-mediaIngles = (li1 + li2 + li3 + li4)/4
-mediaEspanhol = (le1 + le2 + le3 + le4)/4
-mediaArte = (arte1 + arte2 + arte3 + arte4)/4
-mediaEdFisica = (edFis1 + edFis2 + edFis3 + edFis4)/4
-mediaGeografia = (geo1 + geo2 + geo3 + geo4)/4
-mediaHistoria = (his1 + his2 + his3 + his4)/4
-mediaFilosofia = (fil1 + fil2 + fil3 + fil4)/4
-mediaSociologia = (soc1 + soc2 + soc3 + soc4)/4
-mediaFisica = (fis1 + fis2 + fis3 + fis4)/4
-mediaQuimica = (qui1 + qui2 + qui3 + qui4)/4
-mediaBiologia = (bio1 + bio2 + bio3 + bio4)/4
+mediaMatematica = calculaNotas(mt1, mt2, mt3, mt4)
+mediaPortugues = calculaNotas(lp1, lp2, lp3, lp4)
+mediaIngles = calculaNotas(li1, li2, li3, li4)
+mediaEspanhol = calculaNotas(le1, le2, le3, le4)
+mediaArte = calculaNotas(arte1, arte2, arte3, arte4)
+mediaEdFisica = calculaNotas(edFis1, edFis2, edFis3, edFis4)
+mediaGeografia = calculaNotas(geo1, geo2, geo3, geo4)
+mediaHistoria = calculaNotas(his1, his2, his3, his4)
+mediaFilosofia = calculaNotas(fil1, fil2, fil3, fil4)
+mediaSociologia = calculaNotas(soc1, soc2, soc3, soc4)
+mediaFisica = calculaNotas(fis1, fis2, fis3, fis4)
+mediaQuimica = calculaNotas(qui1, qui2, qui3, qui4)
+mediaBiologia = calculaNotas(bio1, bio2, bio3, bio4)
 
 
 # Média por área
 
-mediaNatureza = (mediaBiologia + mediaFisica + mediaQuimica)/3
-mediaHumanas = (mediaFilosofia + mediaSociologia + mediaGeografia + mediaHistoria)/4
-mediaLinguagens = (mediaArte + mediaEspanhol + mediaIngles + mediaPortugues + mediaEdFisica)/5
-mediaCompleta = (mediaBiologia + mediaFisica + mediaQuimica + mediaFilosofia + mediaSociologia + mediaGeografia + mediaHistoria + mediaArte + mediaEspanhol + mediaIngles + mediaPortugues + mediaEdFisica + mediaMatematica)/13
+mediaNatureza = calculaNotas2(mediaBiologia, mediaFisica, mediaQuimica)
+mediaHumanas = calculaNotas(mediaFilosofia, mediaSociologia, mediaGeografia, mediaHistoria)
+mediaLinguagens = calculaNotas3(mediaArte, mediaEspanhol, mediaIngles, mediaPortugues, mediaEdFisica)
+mediaCompleta = calculaNotasGeral(mediaBiologia, mediaFisica, mediaQuimica, mediaFilosofia, mediaSociologia, mediaGeografia, mediaHistoria, mediaArte, mediaEspanhol, mediaIngles, mediaPortugues, mediaEdFisica, mediaMatematica)
 
 
-if alunoOuProf=='Aluno' or 'aluno':
-    print('Suas médias por área são:')
-    print(f'Matemática e Suas Tecnologias: {mediaMatematica}')
-    print(f'Linguagens, Códigos e Suas Tecnologias: {mediaLinguagens}')
-    print(f'Ciências Humanas e Suas Tecnologias: {mediaHumanas}')
-    print(f'Ciências da Natureza e Suas Tecnologias: {mediaNatureza}')
-    print(f'Sua média GERAL é: {mediaCompleta}.')
+if alunoOuProf=='aluno':
+  print('''
+  
+  Suas médias por área são:
+  ''')
+  print(f'''Matemática e Suas Tecnologias: {mediaMatematica}
+  ''')
+  print(f'''Linguagens, Códigos e Suas Tecnologias: {mediaLinguagens}
+  ''')
+  print(f'''Ciências Humanas e Suas Tecnologias: {mediaHumanas}
+  ''')
+  print(f'''Ciências da Natureza e Suas Tecnologias: {mediaNatureza}
+  ''')
+  print(f'''Sua média GERAL é: {mediaCompleta}.
+  ''')
 
-elif alunoOuProf=='professor' or 'Professor' or 'professora' or 'Professora':
-    print('As médias do aluno {aluno} por área são:')
-    print(f'Matemática e Suas Tecnologias: {mediaMatematica}')
-    print(f'Linguagens, Códigos e Suas Tecnologias: {mediaLinguagens}')
-    print(f'Ciências Humanas e Suas Tecnologias: {mediaHumanas}')
-    print(f'Ciências da Natureza e Suas Tecnologias: {mediaNatureza}')
-    print(f'Média GERAL: {mediaCompleta}.')
+elif alunoOuProf=='professor':
+  print('''
+  As médias do aluno por área são:
+  ''')
+  print(f'''Matemática e Suas Tecnologias: {mediaMatematica}
+  ''')
+  print(f'''Linguagens, Códigos e Suas Tecnologias: {mediaLinguagens}
+  ''')
+  print(f'''Ciências Humanas e Suas Tecnologias: {mediaHumanas}
+  ''')
+  print(f'''Ciências da Natureza e Suas Tecnologias: {mediaNatureza}
+  ''')
+  print(f'''Média GERAL: {mediaCompleta}.
+  ''')
+
+print('Espero ter ajudado!')
